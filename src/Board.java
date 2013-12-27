9d1350310e685f800893fc58bec01099b3ed45d4
petermenke <peter.menke22@gmail.com>
2013-12-27 00:03:29 -0800

    Add javadoc comments

--- src/Board.java
+++ src/Board.java
@@ -17,6 +17,9 @@ public class Board {
 	private int apothem = 4;
 	private Texture texture;
 
+	/**
+	 * Create a new Board
+	 */
 	public Board() {
 		board = new byte[28][31];
 		try {
@@ -26,6 +29,9 @@ public class Board {
 		}
 	}
 
+	/**
+	 * Construct board from a .pac file in the levels folder.
+	 */
 	public void readBoardFromFile() {
 		Scanner s = null;
 		try {
@@ -44,6 +50,9 @@ public class Board {
 		}
 	}
 
+	/**
+	 * Display the board with its current contents.
+	 */
 	public void display() {
 		for (int why = 0; why < board[0].length; why++) {
 			for (int ex = 0; ex < board.length; ex++) {
@@ -79,11 +88,26 @@ public class Board {
 		}
 	}
 
+	/**
+	 * Remove coin at the given coordinates.
+	 * This sets the slot the coin was in to value 0
+	 * @param ex The x coordinate of the coin
+	 * @param why The y coordinate of the coin
+	 */
 	public void removeCoinAt(int ex, int why) {
 		board[ex][why] = 0;
 	}
 
-	private void displayTexture(int textureX, int textureY, double boardX, double boardY, int face) {
+	/**
+	 * Display a texture onto the board.
+	 * This displays a portion of the texture specified by the user onto a specified location of the board, also specified by the user.
+	 * @param textureX The x coordinate of the image on the texture
+	 * @param textureY The y coordinate of the image on the texture
+	 * @param boardX The x coordinate to display the image on the board at
+	 * @param boardY The y coordinate to display the image on the board at
+	 * @param rotation The amount of rotation to apply to the image. Either 0, 1, 2, or 3
+	 */
+	private void displayTexture(int textureX, int textureY, double boardX, double boardY, int rotation) {
 		if(textureX != 0 && textureX != 7) {
 			double scaleFactor = (double)Start.WINDOW_HEIGHT / (board[0].length * 8);
 			double newApothem = apothem * scaleFactor;
@@ -96,7 +120,7 @@ public class Board {
 			texture.bind();
 			GL11.glPushMatrix();
 				GL11.glTranslated(boardX, boardY, 0);
-				GL11.glRotated(face * 90, 0, 0, 1);
+				GL11.glRotated(rotation * 90, 0, 0, 1);
 				GL11.glTranslated(-boardX, -boardY, 0);
 				GL11.glBegin(GL11.GL_QUADS);
 					GL11.glTexCoord2d(0 + numX, 0 + numY);            GL11.glVertex2d(boardX - newApothem, boardY - newApothem);
@@ -109,12 +133,23 @@ public class Board {
 		}
 	}
 
+	/**
+	 * Returns the face directly opposite to the one given.
+	 * @param face The face to be evaluated
+	 * @return The face opposite to the one given
+	 */
 	private int getOpposite(int face) {
 		if(face <= 1)
 			return face + 2;
 		return face - 2;
 	}
 
+	/**
+	 * Gives the number of blocks surrounding the block at the given coordinates.
+	 * @param ex The x coordinate of the block to be evaluated
+	 * @param why The y coordinate of the block to be evaluated
+	 * @return The number of blocks surrounding the block at the given coordinates
+	 */
 	private int getSurroundingBlockNumber(int ex, int why) {
 		if (board[ex][why] < 3 || board[ex][why] == 6)
 			return 0;
@@ -148,6 +183,12 @@ public class Board {
 		return count;
 	}
 
+	/**
+	 * Gives the face of the given block that is not a wall, out of bounds, or data value 7.
+	 * @param ex The x coordinate of the block to be evaluated
+	 * @param why The y coordinate of the block to be evaluated
+	 * @return The face of the given block that is open
+	 */
 	private int getOpenFace(int ex, int why) {
 		int num = 0;
 
@@ -182,6 +223,12 @@ public class Board {
 		return num;
 	}
 
+	/**
+	 * Gives the corner of the given block that is not a wall, out of bounds, or data value 7.
+	 * @param ex The x coordinate of the block to be evaluated
+	 * @param why The y coordinate of the block to be evaluated
+	 * @return The corner of the given block that is open
+	 */
 	private int getOpenCorner(int ex, int why) {
 
 		try {
@@ -214,6 +261,12 @@ public class Board {
 		return 4;
 	}
 
+	/**
+	 * Gives the corner of the given block that is a wall, out of bounds, or data value 7.
+	 * @param ex The x coordinate of the block to be evaluated
+	 * @param why The y coordinate of the block to be evaluated
+	 * @return The corner of the block that is not open
+	 */
 	private int getClosedCorner(int ex,int why) {
 		try {
 			if (board[ex - 1][why - 1] >= 3)
@@ -245,6 +298,13 @@ public class Board {
 		return 4;
 	}
 
+	/**
+	 * Checks if a block of a certain type is next to the block at the given coordinates
+	 * @param ex The x coordinate of the block to be evaluated
+	 * @param why The y coordinate of the block to be evaluated
+	 * @param type The type of the block that is being checked for
+	 * @return If the given block is next to a block with the given type
+	 */
 	private boolean isNextTo(int ex, int why, int type) {
 		try {
 			if (board[ex][why - 1] == type)
--- src/Game.java
+++ src/Game.java
@@ -6,15 +6,24 @@ public class Game {
 
 	Board board;
 
+	/**
+	 * Create a new Game
+	 */
 	public Game() {
 		board = new Board();
 		board.readBoardFromFile();
 	}
 
+	/**
+	 * Update the Game
+	 */
 	public void update() {
 
 	}
 
+	/**
+	 * Display the game to the screen
+	 */
 	public void display() {
 		board.display();
 	}
