5b78408b54f32f86cf211afe920e0f94404f50d3
petermenke <peter.menke22@gmail.com>
2013-12-30 14:41:35 -0800

    Add notes.txt for convenience

--- src/Game.java
+++ src/Game.java
@@ -28,9 +28,8 @@ public class Game {
 	 */
 	public void display() {
 		board.display();
-		int[] nums = board.getCoordinatesAt(Board.snapScreenPointToBoardX(Mouse.getX()), Board.snapScreenPointToBoardY(Mouse.getY()), 3, Board.FACE_UP);
-		int nums2[] = board.getCoordinatesAt(nums[0], nums[1], 3, Board.FACE_LEFT);
-		board.drawBoxAt(nums2[0], nums2[1]);
+		int[] nums = board.getCoordinatesAt(Board.snapScreenPointToBoardX(Mouse.getX()), Board.snapScreenPointToBoardY(Mouse.getY()), 0, Board.FACE_UP);
+		board.drawBoxAt(nums[0], nums[1]);
 	}
 
 }
