f43799f855ccace2b094c27f7c494596f964b9a1
Mason <masonfredenberg@gmail.com>
2014-01-15 09:19:24 -0800

    Change PacMan Variables

--- src/PacMan.java
+++ src/PacMan.java
@@ -2,7 +2,8 @@
  * Created by Mason on 1/14/14.
  */
 public class PacMan {
-    private double x, y;
+    private double x;
+    private double y;
     public PacMan(double ex, double why){
         x = ex;
         y = why;
